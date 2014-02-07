f550b1b95013f6428a34185ce1e39da268a829d9
Benjamin Reid <ben@simpleweb.co.uk>
2014-02-07 16:05:03 +0000

    Check to see if Disqus is there before loading it

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 (function(d,t) {
+  if (!!document.getElementById('disqus_thread')) {
     var c=d.createElement(t);s=d.getElementsByTagName(t)[0];
     c.async=1;c.src='http://benjaminreidme.disqus.com/embed.js';
     s.parentNode.insertBefore(c,s)
+  }
 })(document,'script');
