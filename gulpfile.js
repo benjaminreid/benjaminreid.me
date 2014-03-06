2c0b3291df29868bf0bb52a414a75ec12c1d2255
Benjamin Reid <ben@simpleweb.co.uk>
2014-03-06 09:41:36 +0000

    Configure the gulp task for sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,18 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+var paths = {
+  sass: {
+    src: './css/style.scss',
+    dest: './css'
+  }
+};
+
+gulp.task('sass', function() {
+
+  return gulp.src(paths.sass.src)
+    .pipe(sass({ style: 'compressed' }))
+    .pipe(gulp.dest(paths.sass.dest));
+});
 
 gulp.task('default', function() { });
