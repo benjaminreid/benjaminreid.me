4e6485b53bd4ade0aa2b80afffde1ffeec909d39
Benjamin Reid <ben@simpleweb.co.uk>
2015-03-11 14:21:39 +0000

    Fix http script

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function(d,t) {
   if (!!d.getElementById('disqus_thread')) {
     var c=d.createElement(t);s=d.getElementsByTagName(t)[0];
-    c.async=1;c.src='http://benjaminreidme.disqus.com/embed.js';
+    c.async=1;c.src='//benjaminreidme.disqus.com/embed.js';
     s.parentNode.insertBefore(c,s);
   }
 })(document,'script');
