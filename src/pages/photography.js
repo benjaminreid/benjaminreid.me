ac989f8b713e9ad9a9f40c88bb1cb53f03f5ff7c
Benjamin Reid <ben@simpleweb.co.uk>
2019-04-18 13:40:54 +0100

    Importing images

---
+++ src/pages/photography.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import snowLeopards from "../images/snow-leopards.jpg"
+
+const Photography = () => (
+  <Layout>
+    <div>
+      <h1>Photography</h1>
+      <img src={snowLeopards} />
+    </div>
+  </Layout>
+)
+
+export default Photography
