eca2b2866430a0189655b00a992050ccaf625950
Benjamin Reid <ben@simpleweb.co.uk>
2019-04-18 13:20:48 +0100

    Create simple static page

---
+++ src/pages/pens.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Pens = () => (
+  <Layout>
+    <div>
+      <h1>Pens</h1>
+    </div>
+  </Layout>
+)
+
+export default Pens
