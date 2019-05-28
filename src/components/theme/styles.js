a850174bff773042d5ad37a7fe5bdeeab408461a
Benjamin Reid <ben@simpleweb.co.uk>
2019-05-28 14:02:29 +0100

    Import typeface

--- src/components/theme/styles.js
+++ src/components/theme/styles.js
@@ -1,4 +1,8 @@
 import "sanitize.css"
 import { createGlobalStyle } from "styled-components"
 
-export const GlobalStyle = createGlobalStyle``
+export const GlobalStyle = createGlobalStyle`
+  h1, h2, h3, h4, h5, h6 {
+    font-family: Rubik, sans-serif;
+  }
+`
