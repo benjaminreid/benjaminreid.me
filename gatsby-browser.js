b8dd0720c78c17a8f9f4cd2fbbabde04796c3b81
Benjamin Reid <ben@simpleweb.co.uk>
2019-05-29 10:11:07 +0100

    Change theme to dracula and centre content

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 const React = require("react")
 require("typeface-rubik")
-require("prismjs/themes/prism-twilight.css")
+require("dracula-prism/css/dracula-prism.css")
 const Layout = require("./src/components/layout").default
 const Theme = require("./src/components/theme").default
 
--- src/components/post/styles.js
+++ src/components/post/styles.js
@@ -1,3 +1,6 @@
 import styled from "styled-components"
 
-export const Article = styled.article``
+export const Article = styled.article`
+  max-width: 60ch;
+  margin: 0 auto;
+`
