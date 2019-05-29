730bf6a513f211fe3b190c97c3281825bf3a609a
Benjamin Reid <ben@simpleweb.co.uk>
2019-05-29 09:26:57 +0100

    Some base colors

--- src/components/theme/styles.js
+++ src/components/theme/styles.js
@@ -5,4 +5,13 @@ export const GlobalStyle = createGlobalStyle`
   h1, h2, h3, h4, h5, h6 {
     font-family: Rubik, sans-serif;
   }
+
+  body {
+    background-color: ${props => props.theme.colors.grey(700)};
+    color: ${props => props.theme.colors.orange(100)};
+  }
+
+  a {
+    color: ${props => props.theme.colors.orange()};
+  }
 `
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,29 @@
+const createColorPalette = palette => (variant = 500) => palette[variant]
+
 export default {
   spacing: 16,
+  colors: {
+    orange: createColorPalette({
+      100: "#FFF1E8",
+      200: "#FFDDC6",
+      300: "#FFC8A3",
+      400: "#FF9F5E",
+      500: "#FF7619",
+      600: "#E66A17",
+      700: "#99470F",
+      800: "#73350B",
+      900: "#4D2308",
+    }),
+    grey: createColorPalette({
+      100: "#EBEBEB",
+      200: "#CCCCCC",
+      300: "#ADADAD",
+      400: "#707070",
+      500: "#333333",
+      600: "#2E2E2E",
+      700: "#1F1F1F",
+      800: "#171717",
+      900: "#0F0F0F",
+    }),
+  },
 }
